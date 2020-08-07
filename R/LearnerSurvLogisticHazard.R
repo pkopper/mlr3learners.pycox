c64d263b3bd7fc454bfaeb7566c3980fa5772458
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-08-07 10:43:00 +0100

    update tic, loghaz round

--- R/LearnerSurvLogisticHazard.R
+++ R/LearnerSurvLogisticHazard.R
@@ -260,8 +260,8 @@ LearnerSurvLogisticHazard = R6::R6Class("LearnerSurvLogisticHazard",
         # fix for rounding errors
         x[[i]]$pdf = round(1 - surv[, i], 6)
         x[[i]]$pdf = c(x[[i]]$pdf[1], diff(x[[i]]$pdf))
-        x[[i]]$pdf[x[[i]]$pdf < 0.000001] = 0L
-        x[[i]]$pdf[x[[i]]$pdf > 0.999999] = 1L
+        x[[i]]$pdf[x[[i]]$pdf < 0.001] = 0L
+        x[[i]]$pdf[x[[i]]$pdf > 0.999] = 1L
       }
 
       distr = distr6::VectorDistribution$new(
